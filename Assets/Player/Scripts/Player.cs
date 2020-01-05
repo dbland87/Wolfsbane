e5aaf164c60b346cf02ecfe2699f57b03ab6cde6
David Bland <dbland87@gmail.com>
2020-01-04 19:07:08 -0700

    Added player script

---
+++ Assets/Player/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿namespace Player.Scripts
+{
+    public class Player : Valve.VR.InteractionSystem.Player
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
