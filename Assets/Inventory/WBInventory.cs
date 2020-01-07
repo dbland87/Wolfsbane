8edeb79a08978112650f75a67f2d658ddd320256
David Bland <dbland87@gmail.com>
2020-01-06 21:07:23 -0700

    Added Inventory object

---
+++ Assets/Inventory/WBInventory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WBInventory : MonoBehaviour
+{
+
+    private Dictionary<string, string> itemDict;
+
+    public void SetItemByTag(string tag, string item)
+    {
+        itemDict[tag] = item;
+    }
+
+    public string RetrieveItemByTag(string tag)
+    {
+        if (itemDict.TryGetValue(tag, out var value))
+        {
+            return value;
+        }
+        else
+        {
+            Debug.Log("Item does not exist with tag: " + tag);
+            return null;
+        }
+    }
+}
--- Assets/Player/Scripts/WBHand.cs
+++ Assets/Player/Scripts/WBHand.cs
@@ -1,8 +1,36 @@
-﻿namespace Player.Scripts
+﻿using System;
+using UnityEngine;
+using Valve.VR;
+
+namespace Player.Scripts
 {
     public class WBHand : Valve.VR.InteractionSystem.Hand
     {
+
+        public WBInventory Inventory;
+
+        private void MaterializeWeapon(string weaponTag)
+        {
+            Inventory.RetrieveItemByTag(weaponTag);
+        }
+
+        private void DematerializeWeapon()
+        {
+            
+        }
         
-        
+        public void OnGrabGripDown()
+        {
+            if (hoveringInteractable == null)
+            {
+                MaterializeWeapon(handType.ToString());
+            }
+            
+        }
+
+        public void OnGrabGripUp()
+        {
+            DematerializeWeapon();
+        }
     }
 }
