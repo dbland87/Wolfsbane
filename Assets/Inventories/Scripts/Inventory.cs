e2554084011762a0973336ceb9c555265fd27cf6
David Bland <dbland87@gmail.com>
2020-01-08 21:15:05 -0700

    Built a gimped tranform of Inventory, good job dave

--- Assets/Inventories/Scripts/Inventory.cs
+++ Assets/Inventories/Scripts/Inventory.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections.Generic;
 using Items.Scripts;
 using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using UnityEngine.UIElements;
 using Valve.VR.InteractionSystem;
 
 namespace Inventories.Scripts
 {
     public class Inventory : MonoBehaviour
     {
+        public Transform headTransform;
+        
         private readonly Dictionary<string, Item> itemDict = new Dictionary<string, Item>();
 
+        void FixedUpdate()
+        {
+            //TODO this is temp logic
+            transform.localPosition = new Vector3(transform.localPosition.x, -headTransform.position.y / 2, transform.localPosition.z);
+        }
         public void SetItemByTag(string tag, Item item)
         {
             itemDict[tag] = item;
